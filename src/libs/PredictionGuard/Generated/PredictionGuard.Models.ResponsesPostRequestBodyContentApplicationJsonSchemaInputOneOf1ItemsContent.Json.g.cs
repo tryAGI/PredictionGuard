74b5f694208565f47e0b62f55206417314fe1c20
Konstantin S. <havendv@gmail.com>
2026-06-17 14:48:38 +0400

    feat: Updated OpenAPI spec (#30)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/PredictionGuard/Generated/PredictionGuard.Models.ResponsesPostRequestBodyContentApplicationJsonSchemaInputOneOf1ItemsContent.Json.g.cs
+++ src/libs/PredictionGuard/Generated/PredictionGuard.Models.ResponsesPostRequestBodyContentApplicationJsonSchemaInputOneOf1ItemsContent.Json.g.cs
@@ -17,6 +17,14 @@ namespace PredictionGuard
         }
 
         /// <summary>
+        /// Serializes the current instance to a JSON string using the generated default JsonSerializerContext.
+        /// </summary>
+        public string ToJson()
+        {
+            return ToJson(global::PredictionGuard.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Serializes the current instance to a JSON string using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -26,6 +34,11 @@ namespace PredictionGuard
         public string ToJson(
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return ToJson(global::PredictionGuard.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.Serialize(
                 this,
                 jsonSerializerOptions);
@@ -45,6 +58,17 @@ namespace PredictionGuard
         }
 
         /// <summary>
+        /// Deserializes a JSON string using the generated default JsonSerializerContext.
+        /// </summary>
+        public static global::PredictionGuard.ResponsesPostRequestBodyContentApplicationJsonSchemaInputOneOf1ItemsContent? FromJson(
+            string json)
+        {
+            return FromJson(
+                json,
+                global::PredictionGuard.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Deserializes a JSON string using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -55,6 +79,13 @@ namespace PredictionGuard
             string json,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return FromJson(
+                    json,
+                    global::PredictionGuard.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.Deserialize<global::PredictionGuard.ResponsesPostRequestBodyContentApplicationJsonSchemaInputOneOf1ItemsContent>(
                 json,
                 jsonSerializerOptions);
@@ -74,6 +105,17 @@ namespace PredictionGuard
         }
 
         /// <summary>
+        /// Deserializes a JSON stream using the generated default JsonSerializerContext.
+        /// </summary>
+        public static global::System.Threading.Tasks.ValueTask<global::PredictionGuard.ResponsesPostRequestBodyContentApplicationJsonSchemaInputOneOf1ItemsContent?> FromJsonStreamAsync(
+            global::System.IO.Stream jsonStream)
+        {
+            return FromJsonStreamAsync(
+                jsonStream,
+                global::PredictionGuard.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Deserializes a JSON stream using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -84,6 +126,13 @@ namespace PredictionGuard
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return FromJsonStreamAsync(
+                    jsonStream,
+                    global::PredictionGuard.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::PredictionGuard.ResponsesPostRequestBodyContentApplicationJsonSchemaInputOneOf1ItemsContent?>(
                 jsonStream,
                 jsonSerializerOptions);
